85edfbb3b001a8f4f50175e54d11d09f2d650f65
kelechi <oparaiskelechi@gmail.com>
2021-08-12 17:36:35 +0100

    added the nav component

---
+++ app/src/main/java/com/asterisk/gallerywayapp/ui/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package com.asterisk.gallerywayapp.ui
+
+import androidx.fragment.app.Fragment
+import com.asterisk.gallerywayapp.R
+
+class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+
+}
