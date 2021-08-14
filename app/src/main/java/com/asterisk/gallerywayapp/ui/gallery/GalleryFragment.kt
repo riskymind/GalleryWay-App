3d3839f8db08e3e035602f8de77411d14e89730c
kelechi <oparaiskelechi@gmail.com>
2021-08-14 01:28:29 +0100

    could search api

--- app/src/main/java/com/asterisk/gallerywayapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/asterisk/gallerywayapp/ui/gallery/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package com.asterisk.gallerywayapp.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.asterisk.gallerywayapp.R
@@ -9,7 +12,7 @@ import com.asterisk.gallerywayapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     private val viewModel by viewModels<GalleryViewModel>()
 
     private var _binding: FragmentGalleryBinding? = null
@@ -24,14 +27,42 @@ class GalleryFragment: Fragment(R.layout.fragment_gallery) {
 
         binding.apply {
             recyclerView.setHasFixedSize(true)
-            recyclerView.adapter = adapter
+            recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = UnSplashPhotoLoadStateFooter(adapter::retry),
+                footer = UnSplashPhotoLoadStateFooter { adapter.retry() }
+            )
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+
+        setHasOptionsMenu(true)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_gallery, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchQuery(query)
+                    searchView.clearFocus()
+                }
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
